b738fb5bc0dc2382457c1b96ca8144a3f55f537f
ramtaruman <sengupta.prakhar@gmail.com>
2022-11-10 15:26:58 +0530

    First Draft.
    Lots of changes since the first inception, code can now actually track midi patterns properly but it is still slightly restricted to having a 4/4 time signature.
    Needs further testing on percussive implementation.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Midi-Generator
+# midi-generator
+
+Midifile generation using Tensorflow using [Restricted Boltzmann Machine](http://deeplearning4j.org/restrictedboltzmannmachine.html).
---
+++ rbm_chords.py
@@ -0,0 +1,120 @@
+
+
+import os
+import midi_manipulation
+import numpy as np
+import pandas as pd
+import msgpack
+import glob
+import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+from tqdm import tqdm
+tf.compat.v1.disable_eager_execution()
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+def get_songs(path):
+    files = glob.glob('{}/*.mid*'.format(path))
+    songs = []
+    for f in tqdm(files):
+        try:
+            song = np.array(midi_manipulation.midiToNoteStateMatrix(f))
+            if np.array(song).shape[0] > 50:
+                songs.append(song)
+        except Exception as e:
+            raise e
+    return songs
+
+
+songs = get_songs('Pop_Music_Midi')
+print("{} songs processed".format(len(songs)))
+
+
+lowest_note = midi_manipulation.lowerBound
+highest_note = midi_manipulation.upperBound
+note_range = highest_note - lowest_note
+num_timesteps = 15
+n_visible = 2 * note_range * num_timesteps
+n_hidden = 50
+
+num_epochs = 200
+
+batch_size = 100
+lr = tf.constant(0.005, tf.float32)
+
+
+x = tf.compat.v1.placeholder(tf.float32, [None, n_visible], name="x")
+W = tf.Variable(tf.random.normal([n_visible, n_hidden], 0.01), name="W")
+bh = tf.Variable(tf.zeros([1, n_hidden], tf.float32, name="bh"))
+bv = tf.Variable(tf.zeros([1, n_visible], tf.float32, name="bv"))
+
+
+def sample(probs):
+
+    return tf.floor(probs + tf.random.uniform(tf.shape(input=probs), 0, 1))
+
+
+def gibbs_sample(k):
+
+    def gibbs_step(count, k, xk):
+
+        hk = sample(tf.sigmoid(tf.matmul(xk, W) + bh))
+        xk = sample(
+            tf.sigmoid(tf.matmul(hk, tf.transpose(a=W)) + bv))
+        return count + 1, k, xk
+
+    ct = tf.constant(0)
+    [_, _, x_sample] = control_flow_ops.while_loop(lambda count, num_iter, *args: count < num_iter,
+                                                   gibbs_step, [ct, tf.constant(k), x])
+
+    x_sample = tf.stop_gradient(x_sample)
+    return x_sample
+
+
+x_sample = gibbs_sample(1)
+
+h = sample(tf.sigmoid(tf.matmul(x, W) + bh))
+
+h_sample = sample(tf.sigmoid(tf.matmul(x_sample, W) + bh))
+
+size_bt = tf.cast(tf.shape(input=x)[0], tf.float32)
+W_adder = tf.multiply(lr / size_bt,
+                      tf.subtract(tf.matmul(tf.transpose(a=x), h), tf.matmul(tf.transpose(a=x_sample), h_sample)))
+bv_adder = tf.multiply(
+    lr / size_bt, tf.reduce_sum(input_tensor=tf.subtract(x, x_sample), axis=0, keepdims=True))
+bh_adder = tf.multiply(
+    lr / size_bt, tf.reduce_sum(input_tensor=tf.subtract(h, h_sample), axis=0, keepdims=True))
+
+updt = [W.assign_add(W_adder), bv.assign_add(
+    bv_adder), bh.assign_add(bh_adder)]
+
+
+with tf.compat.v1.Session() as sess:
+
+    init = tf.compat.v1.global_variables_initializer()
+    sess.run(init)
+
+    for epoch in tqdm(range(num_epochs)):
+        for song in songs:
+
+            song = np.array(song)
+            song = song[:int(
+                np.floor(song.shape[0] // num_timesteps) * num_timesteps)]
+            song = np.reshape(
+                song, [song.shape[0] // num_timesteps, song.shape[1] * num_timesteps])
+
+            for i in range(1, len(song), batch_size):
+                tr_x = song[i:i + batch_size]
+                sess.run(updt, feed_dict={x: tr_x})
+
+    sample = gibbs_sample(1).eval(session=sess, feed_dict={
+        x: np.zeros((10, n_visible))})
+    for i in range(sample.shape[0]):
+        if not any(sample[i, :]):
+            continue
+
+        S = np.reshape(sample[i, :], (num_timesteps, 2 * note_range))
+        midi_manipulation.noteStateMatrixToMidi(
+            S, "out/generated_chord_{}".format(i))
